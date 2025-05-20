e72069c18f98a912be206124373065e00c8db2d6
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2025-05-20 14:51:12 +0300

    feat: add mobile money action

---
+++ src/main/java/com/prog5/ussd_simulator/action/MobileMoneyAction.java
@@ -0,0 +1,32 @@
+package com.prog5.ussd_simulator.action;
+
+public class MobileMoneyAction {
+    public static void acheterCreditOuOffre(){
+        System.out.println("Acheter Credit ou Offre ");
+    }
+
+    public static void transfererArgent(){
+        System.out.println("Transferer argent");
+    }
+
+    public static void MBCredit() {
+        System.out.println("Mobile money Credit...");
+    }
+
+    public static void retirerArgent() {
+        System.out.println("Retrait d'argent...");
+    }
+
+    public static void monCompte() {
+        System.out.println("Solde MObile money...");
+    }
+
+    public static void recevoirArgent(){
+        System.out.println("recevoir de l'argent");
+    }
+
+    public static void banquesEtMicroFinances(){
+        System.out.println("banques et microfinances");
+    }
+
+}
