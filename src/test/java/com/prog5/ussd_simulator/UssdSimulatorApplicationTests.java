11dbd87be1b77abb87ddb28a8fc11b009aab6748
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2025-05-20 14:48:34 +0300

    feat: initial commit

---
+++ src/test/java/com/prog5/ussd_simulator/UssdSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prog5.ussd_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UssdSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
